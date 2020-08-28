af5abaa322100242ea94ef45bb1badbc41f5342b
Shadee <shadmerhi@gmail.com>
2020-08-28 05:44:11 +0000

    Checking if numbers are odd

--- tinkering.js
+++ tinkering.js
@@ -1,9 +1,11 @@
-const whichSchool = function(age) {
-  if(age < 13) {
-    console.log('Elementary School');
-  } else if (age >= 13 && age <= 18) {
-    console.log('Secondary School');
+for (let i = 100; i <= 200; i++) {
+  if(i % 3 === 0 && i % 4 === 0) {
+    console.log('LoopyLighthouse');
+  } else if (i % 3 === 0) {
+    console.log('Loopy');
+  } else if (i % 4 === 0) {
+    console.log('Lighthouse');
   } else {
-    console.log('Lighthouse Labs');
+    console.log(i);
   }
 }
